1159b77d15667620ec69c677d358eb4f1884b9e7
boyka <abdelrahmanzaki747@gmail.com>
2024-06-19 18:21:35 +0300

    details section

--- src/components/detail/Detail.jsx
+++ src/components/detail/Detail.jsx
@@ -2,8 +2,56 @@ import "./details.css";
 
 const Detail = () => {
   return (
-    <div className="detail">Detail</div>
-  )
-}
+    <div className="detail">
+      <div className="user">
+        <img src="./avatar.png" alt="" />
+        <h2>Rawan Khaled</h2>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Temporibus,
+          harum!
+        </p>
+      </div>
+      <div className="info">
+        <div className="option">
+          <div className="title">
+            <span>Chat Settings</span>
+            <img src="./arrowUp.png" alt="" />
+          </div>
+        </div>
+        <div className="option">
+          <div className="title">
+            <span>Privacy & help</span>
+            <img src="./arrowUp.png" alt="" />
+          </div>
+        </div>
+        <div className="option">
+          <div className="title">
+            <span>Shared photos</span>
+            <img src="./arrowDown.png" alt="" />
+          </div>
+          <div className="photos">
+            <div className="photoItem">
+              <div className="photoDetail">
+                <img
+                  src="https://images.pexels.com/photos/20625279/pexels-photo-20625279/free-photo-of-smling-woman-holding-watermelon-slice.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load"
+                  alt=""
+                />
+                <span>photo_2024</span>
+              </div>
+              <img src="./download.png" alt="" className="icon" />
+            </div>
+          </div>
+        </div>
+        <div className="option">
+          <div className="title">
+            <span>Shared files</span>
+            <img src="./arrowUp.png" alt="" />
+          </div>
+        </div>
+        <button>Block User</button>
+      </div>
+    </div>
+  );
+};
 
-export default Detail
+export default Detail;
