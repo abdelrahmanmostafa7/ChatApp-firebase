8638023d4947dcdb84a5f9cf4babc19ee670b4cd
boyka <abdelrahmanzaki747@gmail.com>
2024-06-22 04:17:38 +0300

    Fetch User Chats from Firestore

--- src/App.jsx
+++ src/App.jsx
@@ -6,26 +6,32 @@ import Login from "./components/login/Login";
 import Notification from "./components/notification/Notification";
 import { onAuthStateChanged } from "firebase/auth";
 import { auth } from "./lib/firebase";
+import { useUserStore } from "./lib/userStore";
+import { useChatStore } from "./lib/chatStore";
+
 const App = () => {
-  const user = false;
+  const { currentUser, isLoading, fetchUserInfo } = useUserStore();
+  const { chatId } = useChatStore();
 
-  useEffect(()=>{
-    const unSub =onAuthStateChanged(auth,(user)=>{
-      console.log(user);
-    })
-    return()=>{
-      unSub()
-    }
-  },[])
+  useEffect(() => {
+    const unSub = onAuthStateChanged(auth, (user) => {
+      fetchUserInfo(user?.uid);
+    });
 
-    
+    return () => {
+      unSub();
+    };
+  }, [fetchUserInfo]);
+
+  if (isLoading) return <div className="loading">Loading...</div>;
+
   return (
     <div className="container">
-      {user ? (
+      {currentUser ? (
         <>
           <List />
-          <Chat />
-          <Detail />
+          {chatId && <Chat />}
+          {chatId && <Detail />}
         </>
       ) : (
         <Login />
--- src/components/list/userInfo/UserInfo.jsx
+++ src/components/list/userInfo/UserInfo.jsx
@@ -1,19 +1,22 @@
-import "./userInfo.css"
+import "./userInfo.css";
+import { useUserStore } from "../../../lib/userStore";
 
-const UserInfo = () => {
+const Userinfo = () => {
+  const { currentUser } = useUserStore();
+
   return (
     <div className="userInfo">
       <div className="user">
-        <img src="avatar.png" alt="" />
-        <h3>Boyka_amz</h3>
+        <img src={currentUser.avatar || "./avatar.png"} alt="" />
+        <h2>{currentUser.username}</h2>
       </div>
       <div className="icons">
-        <img src="./more.png" alt=""  />
+        <img src="./more.png" alt="" />
         <img src="./video.png" alt="" />
         <img src="./edit.png" alt="" />
       </div>
     </div>
   );
-}
+};
 
-export default UserInfo
+export default Userinfo;
