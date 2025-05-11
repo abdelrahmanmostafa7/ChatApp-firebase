92fae95df61fb4b7bb50d66841860839853ca002
boyka <abdelrahmanzaki747@gmail.com>
2025-05-11 16:37:31 +0300

    add user chat

--- src/components/list/chatList/ChatList.jsx
+++ src/components/list/chatList/ChatList.jsx
@@ -1,29 +1,76 @@
-import { useState } from "react";
-import "./chatList.css"
+import { useEffect, useState } from "react";
+import "./chatList.css";
 import AddUser from "./addUser/AddUser";
+import { useUserStore } from "../../../lib/userStore";
+import { doc, getDoc, onSnapshot } from "firebase/firestore";
+import { db } from "../../../lib/firebase";
 
 const ChatList = () => {
-  const [addMode,setAddMode]=useState(false)
+  const [addMode, setAddMode] = useState(false);
+  const { currentUser } = useUserStore();
+  const [chats, setChats] = useState([]);
+
+  useEffect(() => {
+    const unSub = onSnapshot(
+      doc(db, "userChats", currentUser.id),
+      async (res) => {
+        const items = res.data().chats;
+        // const items = res.data()?.chats || [];
+        const promises = items.map(async (item) => {
+          const userDocRef = doc(db, "users", item.receiverId);
+          const userDocSnap = await getDoc(userDocRef);
+
+          const user = userDocSnap.data();
+
+          return { ...item, user };
+        });
+
+        const chatData = await Promise.all(promises);
+
+        // Filter out null or invalid chats
+        setChats(chatData.sort((a, b) => b.updatedAt - a.updatedAt));
+      }
+    );
+    return () => {
+      unSub();
+    };
+  }, [currentUser.id]);
+
+
+
+  
+
   return (
     <div className="chatList">
       <div className="search">
         <div className="searchBar">
           <img src="/search.png" alt="" />
-          <input type="text" placeholder="Search"/>
+          <input type="text" placeholder="Search" />
         </div>
-      <img src={addMode ?"./minus.png" : "./plus.png"} alt="" className="add"
-      onClick={()=> setAddMode((prev)=>!prev)}/>
+        <img
+          src={addMode ? "./minus.png" : "./plus.png"}
+          alt=""
+          className="add"
+          onClick={() => setAddMode((prev) => !prev)}
+        />
       </div>
-      <div className="item">
-        <img src="./avatar.png" alt="" />
-        <div className="texts">
-          <span>Rawan Khaled</span>
-          <p>I Love U ❤</p>
-        </div>
-      </div>
-      {addMode&&<AddUser />}
+
+      {chats.length > 0 ? (
+        chats.map((chat) => (
+          <div className="item" key={chat.chatId || chat.user?.username}>
+            <img src={chat.user?.avatar || "./avatar.png"} alt="avatar" />
+            <div className="texts">
+              <span>{chat.user?.username || "No username"}</span>
+              <p>{chat.lastMessage || "No messages yet"}</p>
+            </div>
+          </div>
+        ))
+      ) : (
+        <p>No chats available</p>
+      )}
+      {addMode && <AddUser />}
     </div>
-  )
-}
+  );
+};
 
 export default ChatList;
